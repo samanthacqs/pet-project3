be544d3aea7e5bcc00f5e39610a52dbc199df752
samanthacqs <samii_karolita@hotmail.com>
2022-02-10 16:50:37 -0500

    last five updated repositories function added

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const getAllItems = async () => {
     const response = await axios.get(BASE);
 
     //const allItems = response.data;
-    //return response.data;
-    console.log(response.data);
+    return response.data;
+    //console.log(response.data);
     //return allItems; //stargazers_count
   } catch (errors) {
     console.error(errors);
@@ -21,3 +21,18 @@ const getAllItems = async () => {
 //   .catch((err) => {
 //     console.error(err);
 //   });
+
+// const moreThanFive = async () => {
+//   const Repos = await getAllItems();
+//   console.log(Repos.filter((Repo) => Repo.stargazers_count > 5));
+// };
+// moreThanFive();
+const lastFiveUp = async () => {
+  const Repos = await getAllItems();
+  const dates = Repos.map((Repo) => Repo.updated_at)
+    .sort()
+    .slice(-5);
+
+  console.log(dates);
+};
+lastFiveUp();
